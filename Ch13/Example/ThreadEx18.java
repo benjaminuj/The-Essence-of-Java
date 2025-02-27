1e82e6c4f29d649ea223b1c14d386dc730dc2387
benjaminuj <chosj1526@gmail.com>
2025-02-27 23:59:15 +0900

    Ch13 쓰레드 thread: 예제 18

--- Ch13/Example/ThreadEx18.java
+++ Ch13/Example/ThreadEx18.java
@@ -2,3 +2,33 @@ package Example;
 
 public class ThreadEx18 {
 }
+
+class ThreadEx18_1 implements Runnable {
+    boolean suspended = false;
+    boolean stopped = false;
+
+    Thread th;
+
+    ThreadEx18_1(String name) {
+        th = new Thread(this, name);
+    }
+    public void run() {
+        String name = th.getName();
+
+        while (!stopped) {
+            if (!suspended) {
+                System.out.println(name);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println(name + " - interrupted");
+                }
+            } else {
+                Thread.yield();
+            }
+        }
+        System.out.println(name + " - stopped");
+    }
+}
+
