2c59ac57a2d939590f029a49000cb14d53efdc4c
benjaminuj <chosj1526@gmail.com>
2025-03-06 23:52:29 +0900

    Ch13 쓰레드 thread: 예제 20

--- Ch13/ThreadEx20.java
+++ Ch13/ThreadEx20.java
@@ -1,4 +1,23 @@
 public class ThreadEx20 {
+    public static void main(String[] args) {
+        ThreadEx20_1 gc = new ThreadEx20_1();
+        gc.setDaemon(true);
+        gc.start();
+
+        int requiredMemory = 0;
+
+        for (int i = 0; i < 20; i++) {
+            requiredMemory = (int) (Math.random() * 10) * 20;
+
+            //필요한 메모리가 사용할 수 있는 양보다 크거나 전체 메모리의 60% 이상을 사용했을 경우 gc를 깨운다
+            if (gc.freeMemory() < requiredMemory || gc.freeMemory() < gc.totalMemory() * 0.4) {
+//                gc.interrupt(); // 잠자고있는 쓰레드 gc를 깨운다
+            }
+
+            gc.usedMemory += requiredMemory;
+            System.out.println("usedMemory: " + gc.usedMemory);
+        }
+    }
 }
 
 class ThreadEx20_1 extends Thread {
@@ -12,7 +31,16 @@ class ThreadEx20_1 extends Thread {
             } catch (InterruptedException e) {
                 System.out.println("Awaken by interrupt().");
             }
+
+            gc(); // garbage collection을 수행한다
+            System.out.println("Garbage Collected. Free Memory :" + freeMemory());
         }
     }
 
+    public void gc() {
+        usedMemory -= 300;
+        if(usedMemory < 0) usedMemory = 0;
+    }
+    public int totalMemory() { return MAX_MEMORY; }
+    public int freeMemory() { return MAX_MEMORY - usedMemory; }
 }
